9e6ab5a109b5c91aca1988e516ccbc07b2d4712d
raherpradipto <80144773+raherpradipto@users.noreply.github.com>
2021-03-18 07:41:11 +0700

    Add files via upload

---
+++ I0320083_Exercise3.4.py
@@ -0,0 +1,5 @@
+#Contoh cara membuat Dictionary pada Python
+
+dict = {'Name':'Raher','Age':18,'Class':'First'}
+print("dict['Name']:",dict['Name'])
+print("dict['Age']:",dict['Age'])
---
+++ I0320083_Exercise3.6.py
@@ -0,0 +1,8 @@
+#Update dictionary Python
+
+dict = {'Name':'Raher','Age':18,'Class':'First'}
+dict['Age'] = 19; #Mengubah entri yang sudah ada
+dict['School'] = "DPS School" #Menambah entri baru
+
+print("dict['Age']:",dict['Age'])
+print("dict['School']:",dict['School'])
